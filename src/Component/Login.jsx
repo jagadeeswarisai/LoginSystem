43d1caa51387025d943261cc145650d12ab7f16e
unknown <your-email@example.com>
2025-05-08 12:50:52 +0530

    Add New Changes

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -11,7 +11,7 @@ function Login({ onSwitchToSignUp }) {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/login", {
+    const response = await fetch("http://loginsystembackend-lg8s.onrender.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
